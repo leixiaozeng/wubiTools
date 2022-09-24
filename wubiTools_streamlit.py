daa77263b3bd34972e3e889bf19e9d43e00bc1ef
leixiaozeng <leixiaozeng@sina.cn>
2022-09-24 16:07:19 +0800

    修改菜单，添加图标

--- wubiTools_streamlit.py
+++ wubiTools_streamlit.py
@@ -33,10 +33,15 @@ def get_jiegou(url):#获取结果
         st.info(f'{zi}的五笔编码：'+data[0])
         st.image(f"https://c.cidianwang.com/file/wubi/{zi}.gif")
 try:
-    st.set_page_config("五笔查询小工具")
+    menu_dic = {
+        "Get Help":None,
+        "Report a bug":None,
+        "About":"测试版本0.1",
+    }
+    st.set_page_config(page_title="五笔查询小工具",page_icon=":book:",menu_items=menu_dic)
     with st.form("table_1"):
-        zi = st.text_input('输入要查询的文字（只能输入一个字，就算你输入多个也只查询第一个字）',max_chars=6,help='请只输入“一个”字。')
-        if st.form_submit_button("查询"):
+        zi = st.text_input(label='输入要查询的文字（只能输入一个字，就算你输入多个也只查询第一个字)',max_chars=6,help='请只输入“一个”字。')
+        if st.form_submit_button('查询'):
             get_bianma()
 except:
-    st.info('出错了！！确认你输入是否正确哟！')
+    st.info('出错了！！')
